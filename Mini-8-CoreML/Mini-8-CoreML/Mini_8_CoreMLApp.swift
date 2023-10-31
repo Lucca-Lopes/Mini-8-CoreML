01e1480b5fa6036974d173589174e34dc16d95d0
Lucca-Lopes <luccavlopes@hotmail.com>
2023-10-31 17:54:54 -0300

    Feat: projeto adicionado

---
+++ Mini-8-CoreML/Mini-8-CoreML/Mini_8_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_8_CoreMLApp.swift
+//  Mini-8-CoreML
+//
+//  Created by Lucca Lopes on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini_8_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
